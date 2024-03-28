da78270a665250f6a294731f187c2b396d77b910
Shoishob Al-Baki <84293221+Shoishob007@users.noreply.github.com>
2024-03-29 03:31:43 +0600

    register api and route created and tested

---
+++ api/controllers/authController.js
@@ -0,0 +1,21 @@
+import User from "../models/userModel.js"
+import bcryptjs from "bcryptjs";
+
+export const register = async (req, res) => {
+    const { userName, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+
+    const newUser = new User({ userName, email, password: hashedPassword });
+
+    try {
+
+        await newUser.save();
+        res.status(201).json({ message: "User created successfully!" })
+
+    } catch (error) {
+
+        res.status(500).json(error.message)
+
+    }
+
+};
--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 import express from "express"
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js"
+import authRoutes from "./routes/authRoutes.js"
 
 dotenv.config()
 
@@ -10,7 +12,12 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err)
 })
 const app = express();
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log("Server listened at port 3000!")
 })
+
+
+app.use("/api/user", userRoutes)
+app.use("/api/auth", authRoutes)

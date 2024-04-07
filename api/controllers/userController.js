79b129d639f69b370653456892b15cfd9e16d9e9
Shoishob Al-Baki <84293221+Shoishob007@users.noreply.github.com>
2024-04-08 02:58:03 +0600

    done backend for the profile update

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,5 +1,42 @@
+import User from "../models/userModel.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
 export const test = (req, res) => {
     res.json({
         message: "API is working fine!"
     })
 };
+
+//Updating user
+
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) {
+        return next(errorHandler(401, "You can only update your own profile!"))
+    }
+
+    try {
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10)
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: {
+                    userName: req.body.userName,
+                    email: req.body.email,
+                    password: req.body.password,
+                    profilePicture: req.body.profilePicture
+
+                }
+            },
+            { new: true }
+        )
+        const { password, ...rest } = updatedUser._doc
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error)
+    }
+
+}
